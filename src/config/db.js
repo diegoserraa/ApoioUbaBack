0c014891872528b7277234833646968e0d5ed4da
Diego Serra <pralapraka@outlook.com>
2026-02-26 00:09:50 -0300

    teste

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
+const isRender = process.env.RENDER === 'true'; // ✅ variável para detectar Render
+
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: false },
-  host: process.env.DB_URL.split('@')[1].split(':')[0], // força IPv4
+  ssl: isRender ? { rejectUnauthorized: false } : false, // SSL só no Render
 });
 
 async function query(text, params) {

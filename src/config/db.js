dc8a3c27c7f52640e801c179e340d27529ee58cd
Diego Serra <pralapraka@outlook.com>
2026-02-25 23:59:07 -0300

    teste

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DB_URL,
   ssl: { rejectUnauthorized: false },
+  host: process.env.DB_URL.split('@')[1].split(':')[0], // força IPv4
 });
 
 async function query(text, params) {

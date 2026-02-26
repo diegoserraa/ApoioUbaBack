59822d8e8adf3a7a2dee8cb58fc3a7b9dc2da60e
Diego Serra <pralapraka@outlook.com>
2026-02-26 09:04:09 -0300

    teste10

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,8 @@ const { createClient } = require('@supabase/supabase-js');
 // Cria client do Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,      // URL do Supabase
-  process.env.SUPABASE_ANON_KEY  // Chave anon
+  process.env.SUPABASE_ANON_KEY, // Chave anon
+{ global: { headers: { 'X-Client-Info': 'ApoioUbá/1.0' } } }
 );
 
 module.exports = { supabase };
